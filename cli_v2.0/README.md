e8ebfa4a51a20fd3073b3a5f1d3a348b72fb486e
Jose Maria Hidalgo Garcia <jhidalgo3@gmail.com>
2020-12-07 18:13:30 +0100

    Add v2.0 using Ookla Speedtest official CLI

---
+++ cli_v2.0/README.md
@@ -0,0 +1,40 @@
+# Speedtest to InfluxDB
+
+[ORIGINAL SOURCE](https://github.com/aidengilmartin/speedtest-to-influxdb.git)
+
+This is a small Python script that will continuously run the Speedtest CLI application by Ookla, reformat the data output and forward it on to an InfluxDB database.
+
+You may want to do this so that you can track your internet connections consistency over time. Using Grafana you can view and explore this data easily.
+
+![Grafana Dashboard](https://i.imgur.com/8cUdMy7.png)
+
+## Using the script
+
+Adjust the InfluxDB connection settings at the top of `main.py` to fit your setup and then run with one of the options listed below.
+
+Be aware that this script will automatically accept the license and GDPR statement so that it can run non-interactively. Make sure you agree with them before running.
+
+### 1. No Container
+
+1. [Install the Speedtest CLI application by Ookla.](https://www.speedtest.net/apps/cli)
+
+    NOTE: The `speedtest-cli` package in distro repositories is an unofficial client. It will need to be uninstalled before installing the Ookla Speedtest CLI application with the directions on their website.
+
+2. Install the InfluxDB client for library from Python.
+
+    `pip install influxdb`
+
+3. Run the script.
+
+    `python3 ./main.py`
+
+### 2. Run with Docker or Podman
+
+1. Build the container.
+
+    `docker build -t aidengilmartin/speedtest-influx ./`
+
+2. Run the container.
+
+    `docker run -d --name speedtest-influx aidengilmartin/speedtest-influx`
+
---
+++ cli_v2.0/main.py
@@ -0,0 +1,118 @@
+import time
+import json
+import subprocess
+
+from influxdb import InfluxDBClient
+
+# InfluxDB Settings
+DB_ADDRESS = '192.168.2.8'
+DB_PORT = 8086
+DB_USER = 'internal'
+DB_PASSWORD = 'GaPw9nZQ'
+DB_DATABASE = 'speedtest'
+DB_RETRY_INVERVAL = 60 # Time before retrying a failed data upload.
+
+# Speedtest Settings
+TEST_INTERVAL = 86400  # Time between tests (in seconds).
+TEST_FAIL_INTERVAL = 60  # Time before retrying a failed Speedtest (in seconds).
+
+influxdb_client = InfluxDBClient(
+    DB_ADDRESS, DB_PORT, DB_USER, DB_PASSWORD, None)
+
+
+def init_db():
+    try:
+        databases = influxdb_client.get_list_database()
+    except:
+        print("Error: Unable to get list of databases")
+        raise RuntimeError("No DB connection") from error
+    else:
+        if len(list(filter(lambda x: x['name'] == DB_DATABASE, databases))) == 0:
+            influxdb_client.create_database(
+                DB_DATABASE)  # Create if does not exist.
+        else:
+            influxdb_client.switch_database(DB_DATABASE)  # Switch to if does exist.
+
+
+def format_for_influx(cliout):
+    data = json.loads(cliout)
+    # There is additional data in the speedtest-cli output but it is likely not necessary to store.
+    influx_data = [
+        {
+            'measurement': 'ping',
+            'time': data['timestamp'],
+            'fields': {
+                'jitter': float(data['ping']['jitter']),
+                'latency': float(data['ping']['latency'])
+            }
+        },
+        {
+            'measurement': 'download',
+            'time': data['timestamp'],
+            'fields': {
+                # Byte to Megabit
+                'bandwidth': data['download']['bandwidth'] / 125000,
+                'bytes': data['download']['bytes'],
+                'elapsed': data['download']['elapsed']
+            }
+        },
+        {
+            'measurement': 'upload',
+            'time': data['timestamp'],
+            'fields': {
+                # Byte to Megabit
+                'bandwidth': data['upload']['bandwidth'] / 125000,
+                'bytes': data['upload']['bytes'],
+                'elapsed': data['upload']['elapsed']
+            }
+        },
+        {
+            'measurement': 'packetLoss',
+            'time': data['timestamp'],
+            'fields': {
+                'packetLoss': float(data['packetLoss'])
+            }
+        }
+    ]
+            
+    return influx_data
+
+
+def main():
+    db_initialized = False
+    
+    while(db_initialized == False): 
+        try:
+            init_db()  # Setup the database if it does not already exist.
+        except:
+            print("Error: DB initialization error")
+            time.sleep(DB_RETRY_INVERVAL)
+        else:
+            print("Info: DB initialization complete")
+            db_initialized = True
+        
+        
+    while (1):  # Run a Speedtest and send the results to influxDB indefinitely.
+        speedtest = subprocess.run(
+            ["speedtest", "--accept-license", "--accept-gdpr", "-f", "json"], capture_output=True)
+
+        if speedtest.returncode == 0:  # Speedtest was successful.
+            data = format_for_influx(speedtest.stdout)
+            print("Info: Speedtest successful")
+            try:
+                if influxdb_client.write_points(data) == True:
+                    print("Info: Data written to DB successfully")
+                    time.sleep(TEST_INTERVAL)
+            except:
+                print("Error: Data write to DB failed")
+                time.sleep(TEST_FAIL_INTERVAL)
+        else:  # Speedtest failed.
+            print("Error: Speedtest failed")
+            print(speedtest.stderr)
+            print(speedtest.stdout)
+            time.sleep(TEST_FAIL_INTERVAL)
+
+
+if __name__ == '__main__':
+    print('Speedtest CLI Data Logger to InfluxDB')
+    main()
